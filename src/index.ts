974b157cb201b110451a6ef0ae230d042f256be0
Derek Nguyen <derek@penandpillow.com>
2019-05-28 14:23:36 +0900

    update walk so it behave more intuitively & correctly

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,26 @@
 import { choose, chooseOf, chooseWithout } from './utils'
 
-interface WalkerOptions {
-  gridSize: number;
-  maxBlockSize?: number;
-}
-
 export interface Position {
   begin: number;
   end: number;
 }
 
-type WalkOnce = (previous: Position) => Position
-type Walk = (amount: number) => Position[]
-type MakePosition = () => Position
+export interface WalkerOpts {
+  gridSize: number;
+  maxBlockSize?: number;
+}
+
+export interface WalkOpts {
+  fromPos?: Position;
+}
+
+type WalkOnce = (fromPos: Position) => Position
+type Walk = (amount: number, opts?: WalkOpts) => Position[]
 
 class Walker {
   public lines: number
   public maxBlockSize: number
-  public constructor (opts: WalkerOptions) {
+  public constructor (opts: WalkerOpts) {
     const gridSize = Math.max(opts.gridSize, 3)
     this.lines = gridSize + 1
     this.maxBlockSize = opts.maxBlockSize
@@ -25,32 +28,36 @@ class Walker {
       : gridSize
   }
   
-  public walkOnce: WalkOnce = previous => {
+  public walkOnce: WalkOnce = fromPos => {
     const { lines, maxBlockSize } = this
-    const prev = previous || {
-      begin: 0,
-      end: choose(maxBlockSize + 1),
-    }
-    const random = chooseOf(prev)
+    const random = chooseOf(fromPos)
     const randomValue = chooseWithout(lines, {
-      exceptions: Object.values(prev)
+      exceptions: Object.values(fromPos)
     })
 
     const next = {
-      ...prev,
+      ...fromPos,
       [random]: randomValue
     }
 
     return (Math.abs(next.begin - next.end) > maxBlockSize)
-      ? this.walkOnce(prev)
+      ? this.walkOnce(fromPos)
       : next
   }
 
-  public walk: Walk = amount => {
-    const { walkOnce } = this
+  public walk: Walk = (amount, opts) => {
+    const { walkOnce, maxBlockSize } = this
+
+    const first = opts && opts.fromPos || {
+      begin: 0,
+      end: choose(maxBlockSize + 1),
+    }
+    
     return Array(amount)
       .fill(0)
-      .reduce(acc => [walkOnce(acc[0]), ...acc], [])
+      .reduce(acc => [walkOnce(acc[0]), ...acc], [ first ])
+      .slice(1)
+      .reverse()
   }
 }
 
